3d7ecb2befb8f655ab0ac0cbe4ea9bf95c9d17da
Joseph Weissman <jweissman@pivotal.io>
2019-12-06 15:01:40 -0500

    initial commit!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classnames from 'classnames';
+import './App.scss';
+
+class Button extends React.Component<{
+  primary?: boolean,
+  size?: 'large' | 'small',
+}> {
+  render() {
+    let size = this.props.size || 'medium';
+    return <button className={classnames([
+      "Button",
+      (this.props.primary ? 'Button--primary' : ''),
+      (this.props.size === 'large' ? 'Button--large' : ''),
+    ])}>
+      {this.props.children}
+    </button>
+  }
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome to Krypton
       </header>
+      <main className="App-content">
+        <p>the app content</p>
+        <p>card</p>
+        <div className="Card">
+          <div className="Card-title">
+            card title
+          </div>
+          <div className="Card-body">
+            card body
+          </div>
+        </div>
+        {/* <Card>
+          <CardTitle></CardTitle>
+          <CardBody></CardBody>
+        </Card> */}
+
+        <p>small button</p>
+        <Button>
+          Hi!
+        </Button>
+        <p>large primary button</p>
+        <Button primary size="large">
+          Hi!
+        </Button>
+      </main>
+      <footer className="App-footer">
+        the app footer
+      </footer>
     </div>
   );
 }
